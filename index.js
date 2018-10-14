b2038a88f11eef576d5162c0b0411430c0be1b61
Daniel Lima <daniel.echuaca.lima@gmail.com>
2018-10-14 13:31:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ Cream: '250 ml'
 }
 
 function updateObjectWithKeyAndValue(object, key, value){
- var newObj = Object.assign({},object,{key: value})
+ var newObj = Object.assign({},object,{[key]: value})
  return newObj
 }
 
