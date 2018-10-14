4f9987a7cae0e4e1870fc8e249ec7d2852cadbb6
Daniel Lima <daniel.echuaca.lima@gmail.com>
2018-10-14 13:41:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+   var newObj = Object.assign({},object,{[key]: value})
+   delete newObj.key
+   return newObj
 }
 
 
